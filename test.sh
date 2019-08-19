c473430f55025b83fe66065a7f1ccdcb64f01add
yishaihl <yishaihl@gmail.com>
2019-08-19 16:44:40 +0300

    Added test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#Start docker quickstart termial:
+/Users/yishaihalpert/Documents/GitHub/Final-Project/start.sh
+
+#Configure environment vairable:
+eval $(docker-machine env default)
+
+#CD to project folder: 
+cd /Users/yishaihalpert/Documents/GitHub/Final-Project/
+
+#Docker compose: 
+docker-compose up -d
+
+
